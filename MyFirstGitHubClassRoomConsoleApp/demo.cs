bb7ce5d28b2b3fb99133fed8aa7ed950475036a8
ToniK4 <toni.kneze4@gmail.com>
2021-09-01 08:30:29 +0200

    Made class, properties and constructor

---
+++ MyFirstGitHubClassRoomConsoleApp/demo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class demo
+    {
+        public int Integer { get; set; }
+        public string String { get; set; }
+        public demo()
+        {
+            
+        }
+    }
+}
